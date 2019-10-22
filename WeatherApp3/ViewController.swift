454e04345c164d14e84193b1a5293e6aeedb69be
Student <student@imac-06.local>
2019-10-22 10:55:38 +0200

    first

--- WeatherApp3/ViewController.swift
+++ WeatherApp3/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    let locationManager = CLLocationManager()
+    
     var curr = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways){
+            locationManager.requestLocation()
+        }
+        
         temperatura.text = "Temp"
         
         let url = URL(string: "/static/img/weather/png/64/hr.png")!
@@ -21,20 +30,46 @@ class ViewController: UIViewController {
             if error == nil && data != nil {
                 self.ikona.image = UIImage(data: data!)
             }
-            }}
-        )
+        }
+            
+    }
+    )
         
 
         session.resume()
         nextBtn.isEnabled = true
         
+}
+
+    
+    
+    func retriveCurrentLocation(){
+        let status = CLLocationManager.authorizationStatus()
+        
+        if(status == .denied || status == .restricted || !CLLocationManager.locationServicesEnabled()){
+            // show alert to user telling them they need to allow location data to use some feature of your app
+            return
+        }
+        
+        // if haven't show location permission dialog before, show it to user
+        if(status == .notDetermined){
+            locationManager.requestWhenInUseAuthorization()
+            
+            // if you want the app to retrieve location data even in background, use requestAlwaysAuthorization
+            // locationManager.requestAlwaysAuthorization()
+            return
+        }
+        
+        // at this point the authorization status is authorized
+        // request location data once
+        locationManager.requestLocation()
+        
+        // start monitoring location data and get notified whenever there is change in location data / every few seconds, until stopUpdatingLocation() is called
+        // locationManager.startUpdatingLocation()
     }
     @IBOutlet weak var temperatura: UILabel!
-    
     @IBOutlet weak var ikona: UIImageView!
-    
     @IBOutlet weak var nextBtn: UIButton!
-    
     @IBAction func Next(_ sender: Any) {
         if curr == 7{
             nextBtn.isEnabled = false
@@ -45,7 +80,6 @@ class ViewController: UIViewController {
         }
     }
     @IBOutlet weak var Data: UILabel!
-    
     @IBAction func Prev(_ sender: Any) {
         if curr == -7{ PrevBtn.isEnabled = false }
         else{
@@ -54,5 +88,24 @@ class ViewController: UIViewController {
         }
     }
     @IBOutlet weak var PrevBtn: UIButton!
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("location manager authorization status changed")
+        
+        switch status {
+        case .authorizedAlways:
+            print("user allow app to get location data when app is active or in background")
+        case .authorizedWhenInUse:
+            print("user allow app to get location data only when app is active")
+        case .denied:
+            print("user tap 'disallow' on the permission dialog, cant get location data")
+        case .restricted:
+            print("parental control setting disallow location data")
+        case .notDetermined:
+            print("the location permission dialog haven't shown before, user haven't tap allow/disallow")
+        }
+    }
 }
 
